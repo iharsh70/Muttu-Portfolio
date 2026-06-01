ab201b70c6d811460915239d6e0d806d461c961d
iharsh70 <harshimmadiharsh@gmail.com>
2026-06-01 16:09:50 +0530

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Muttu-Portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
